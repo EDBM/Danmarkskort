a540f694b4d953a3f762a686e97f90783141aa71
jpor <jpor@itu.dk>
2020-03-21 21:28:33 +0100

    Add MapCanvas class. Has the responsibility of drawing the Map itself

--- src/main/java/BFST20Project/View.java
+++ src/main/java/BFST20Project/View.java
@@ -18,65 +18,21 @@ import java.util.List;
 public class View {
     Model model;
     Stage stage;
-    Canvas canvas = new Canvas(640,480);
-    GraphicsContext gc = canvas.getGraphicsContext2D();
+    MapCanvas canvas = new MapCanvas(640, 480);
     BorderPane pane = new BorderPane(canvas);
     Scene scene = new Scene(pane);
-    Affine trans = new Affine();
-    ColorScheme colorScheme = new DefaultColorScheme();
 
     public View(Model model, Stage primaryStage){
         this.model=model;
         this.stage=primaryStage;
-        model.addObserver(this::repaint);
         stage.setScene(scene);
         stage.show();
-        resetView();
-        canvas.widthProperty().bind(scene.widthProperty());
+        canvas.init(model);
+        /*canvas.widthProperty().bind(scene.widthProperty());
         canvas.heightProperty().bind(scene.heightProperty());
         canvas.widthProperty().addListener((a,b,c) -> repaint());
-        canvas.heightProperty().addListener((a,b,c) -> repaint());
+        canvas.heightProperty().addListener((a,b,c) -> repaint());*/
     }
 
-    public void resetView() {
-        pan(-model.minLon, -model.minLat);
-        zoom(canvas.getWidth() / (model.maxLat - model.minLat), 0, 0);
-        repaint();
-    }
 
-    public void repaint(){
-        gc.setTransform(new Affine());
-        gc.setFill(Color.LIGHTBLUE);
-        gc.fillRect(0,0,canvas.getWidth(),canvas.getHeight());
-        gc.setTransform(trans);
-        gc.setFill(Color.LIGHTGREEN);
-        gc.setStroke(Color.BLACK);
-
-        double pixelWidth = 1/Math.sqrt(Math.abs(trans.determinant()));
-        gc.setLineWidth(pixelWidth);
-
-        EnumMap<WayType, List<Drawable>> drawables = model.getDrawables();
-        for (WayType type : drawables.keySet()){
-            gc.setStroke(colorScheme.getStroke(type));
-            boolean shouldFill = colorScheme.shouldFill(type);
-            if(shouldFill){
-                gc.setFill(colorScheme.getFill(type));
-            }
-            for(Drawable drawable : drawables.get(type)){
-                drawable.stroke(gc);
-                if(shouldFill)
-                    drawable.fill(gc);
-            }
-
-        }
-    }
-
-    public void pan(double dx, double dy) {
-        trans.prependTranslation(dx, dy);
-        repaint();
-    }
-    public void zoom(double factor, double x, double y) {
-        trans.prependScale(factor, factor, x, y);
-        repaint();
-    }
 }
