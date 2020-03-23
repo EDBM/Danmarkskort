9d91897dd2bf6328fc0d330a80064460c864a451
jpor <jpor@itu.dk>
2020-03-23 20:30:34 +0100

    Changed the internal workings of the ColorScheme.

--- src/main/java/BFST20Project/DefaultColorScheme.java
+++ src/main/java/BFST20Project/DefaultColorScheme.java
@@ -3,34 +3,30 @@ package BFST20Project;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 
+import java.util.EnumMap;
+import java.util.Map;
+
 public class DefaultColorScheme extends ColorScheme {
-    public Paint getStroke(WayType wayType) {
-        switch (wayType){
-            case HIGHWAY:
-                return Color.BLACK;
-            case BUILDING:
-                return Color.BEIGE;
-            case WATER:
-                return Color.BLUE;
-            case FOREST:
-                return Color.GREEN;
-            default:
-                return Color.RED;
-        }
+    protected Paint defaultColor(){
+        return Color.RED;
     }
 
-    public Paint getFill(WayType wayType) {
-        switch (wayType){
-            case BUILDING:
-                return Color.BEIGE;
-            case WATER:
-                return Color.BLUE;
-            case FOREST:
-                return Color.GREEN;
-            default:
-                return Color.RED;
-        }
+    protected Map<WayType, Paint> createStroke() {
+        EnumMap<WayType, Paint> resultMap = new EnumMap<>(WayType.class);
+        resultMap.put(WayType.HIGHWAY, Color.BLACK);
+        resultMap.put(WayType.BUILDING, Color.BEIGE);
+        resultMap.put(WayType.WATER, Color.BLUE);
+        resultMap.put(WayType.FOREST, Color.GREEN);
+
+        return resultMap;
     }
 
+    protected Map<WayType, Paint> createFill() {
+        EnumMap<WayType, Paint> resultMap = new EnumMap<>(WayType.class);
+        resultMap.put(WayType.BUILDING, Color.BEIGE);
+        resultMap.put(WayType.WATER, Color.BLUE);
+        resultMap.put(WayType.FOREST, Color.GREEN);
 
+        return resultMap;
+    }
 }
