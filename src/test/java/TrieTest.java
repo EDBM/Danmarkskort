11e71ed7f475095d6606a17ec9edbf1d37fd7dec
luvu@itu.dk <61692204moR>
2020-04-12 03:15:25 +0200

    Automatic testing of Trie autocomplete

--- src/test/java/TrieTest.java
+++ src/test/java/TrieTest.java
@@ -1,21 +1,22 @@
 import BFST20Project.AddressParser;
 import BFST20Project.Trie;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+//import org.junit.Test;
 
 import java.util.List;
 
 public class TrieTest {
 
     @Test
-    public static void TrieAutoCompleteTest(String[] args) {
+    public void TrieAutoCompleteTest() {
         Trie t = new Trie();
         AddressParser a = new AddressParser("Torpenvej", "23", "3050", "København");
         AddressParser b = new AddressParser("Torpengade", "25", "3040", "Købenby");
         t.insert(a);
         t.insert(b);
         List l = t.autocomplete("Torpen");
-        for (int i = 0; i < l.size(); i++) {
-            System.out.println(l.get(i));
-        }
+        Assertions.assertEquals(String.valueOf(l.get(0)), "Torpenvej 23 3050 København");
+        Assertions.assertEquals(String.valueOf(l.get(1)), "Torpengade 25 3040 Købenby");
     }
 }
